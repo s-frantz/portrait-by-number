d12d2a0b1652d7b797a4a34237a063e0c50df3d2
Silas Frantz <silas.frantz@gmail.com>
2024-08-26 13:22:27 -0400

    removed PIL dependency from face_seg.py

--- flask/utils/ai/face_seg.py
+++ flask/utils/ai/face_seg.py
@@ -1,6 +1,5 @@
 from os import path as osp
 import numpy as np
-from PIL import Image
 import cv2
 import torch
 import torchvision.transforms as transforms
@@ -15,7 +14,7 @@ MODEL = osp.join(
 )
 
 
-def vis_parsing_maps(im, parsing_anno, stride):
+def vis_parsing_maps(im, parsing_anno):
 
     # Colors for all 20 parts
     part_colors = [
@@ -45,11 +44,18 @@ def vis_parsing_maps(im, parsing_anno, stride):
         [170, 255, 255]
     ]
 
-    im = np.array(im)
     vis_im = im.copy().astype(np.uint8)
     vis_parsing_anno = parsing_anno.copy().astype(np.uint8)
-    vis_parsing_anno = cv2.resize(vis_parsing_anno, None, fx=stride, fy=stride, interpolation=cv2.INTER_NEAREST)
-    vis_parsing_anno_color = np.zeros((vis_parsing_anno.shape[0], vis_parsing_anno.shape[1], 3)) + 255
+    vis_parsing_anno = cv2.resize(
+        src=vis_parsing_anno,
+        dsize=None,
+        fx=1,
+        fy=1,
+        interpolation=cv2.INTER_NEAREST
+    )
+    vis_parsing_anno_color = 255 + np.zeros(
+        shape=(vis_parsing_anno.shape[0], vis_parsing_anno.shape[1], 3)
+    )
 
     num_of_class = np.max(vis_parsing_anno)
 
@@ -58,16 +64,34 @@ def vis_parsing_maps(im, parsing_anno, stride):
         vis_parsing_anno_color[index[0], index[1], :] = part_colors[pi]
 
     vis_parsing_anno_color = vis_parsing_anno_color.astype(np.uint8)
-    vis_im = cv2.addWeighted(cv2.cvtColor(vis_im, cv2.COLOR_RGB2BGR), 0.4, vis_parsing_anno_color, 0.6, 0)
+    vis_im = cv2.addWeighted(
+        src1=cv2.cvtColor(vis_im, cv2.COLOR_RGB2BGR),
+        alpha=0.4,
+        src2=vis_parsing_anno_color,
+        beta=0.6,
+        gamma=0
+    )
 
     return vis_im, vis_parsing_anno
 
 
-def evaluate(impath):
+def evaluate(im):
 
+    # prep image
+    if type(im) == str:
+        im = cv2.cvtColor(cv2.imread(im), cv2.COLOR_BGR2RGB)
+    im_resize = cv2.resize(im, (512, 512))
+
+    # define model & transform
     n_classes = 19
     net = BiSeNet(n_classes=n_classes)
-    net.load_state_dict(torch.load(MODEL, map_location=torch.device('cpu'), weights_only=True))
+    net.load_state_dict(
+        state_dict = torch.load(
+            f=MODEL,
+            map_location=torch.device('cpu'),
+            weights_only=True
+        )
+    )
     net.eval()
 
     to_tensor = transforms.Compose([
@@ -76,22 +100,16 @@ def evaluate(impath):
     ])
 
     with torch.no_grad():
+        
+        t = to_tensor(im_resize)
 
-        if type(impath) == str:
-            img = Image.open(impath)
-        else:
-            img = impath
-            
-        image = img.resize((512, 512), Image.BILINEAR)
-        img = to_tensor(image)
-        img = torch.unsqueeze(img, 0)
-        out = net(img)[0]
+        t = torch.unsqueeze(t, 0)
+        out = net(t)[0]
         parsing = out.squeeze(0).cpu().numpy().argmax(0)
 
         return vis_parsing_maps(
-            image,
-            parsing,
-            stride=1,
+            im_resize,
+            parsing
         )
 
 
